cdfd830e9c690581e5fcda55593c5b59834a5c44
Kevin LeRoy <kevin@kevinleroy.me>
2020-11-03 12:40:44 -0800

    Added redux to ProductScreen.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,14 @@ import data from './data.js';
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.get('/api/products/:id', (req, res) => {
+	const product = data.products.find((x) => x._id === req.params.id);
+	if (product) {
+		res.send(product);
+	} else {
+		res.status(404).send({ message: 'Product Not Found' });
+	}
+});
 
 app.get('/api/products', (req, res) => {
 	res.send(data.products);
@@ -12,6 +19,8 @@ app.get('/api/products', (req, res) => {
 app.get('/', (req, res) => {
 	res.send('Server is ready');
 });
+
+const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
 	console.log(`Server at localhost:${port}`);
