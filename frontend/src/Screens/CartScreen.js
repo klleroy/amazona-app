eb4184b6350285413429e6cecdc5b05953c169cb
Kevin LeRoy <kevin@kevinleroy.me>
2020-11-03 13:56:53 -0800

    Implement Add to Cart.

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location }) => {
 	const productId = match.params.id;
 	const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (productId) {
+			dispatch(addToCart(productId, qty));
+		}
+	}, [dispatch, productId, qty]);
 	return (
 		<div>
 			<h1>Cart Screen</h1>
