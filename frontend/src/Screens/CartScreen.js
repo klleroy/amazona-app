8e67f6265e3b93c0302fec37d72495c3ed9dc712
Kevin LeRoy <kevin@kevinleroy.me>
2020-11-03 15:26:19 -0800

    Implement 'Remove from Cart'

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 import { Link } from 'react-router-dom';
 
@@ -17,7 +17,7 @@ const CartScreen = ({ match, location }) => {
 	}, [dispatch, productId, qty]);
 
 	const removefromCartHandler = (id) => {
-		// delete action
+		dispatch(removeFromCart(id));
 	};
 
 	const checkoutHandler = ({ history }) => {
